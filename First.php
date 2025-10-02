c8fff4e240e35906b3ba4fca66f72dd85c89b033
mybaby <example.com>
2025-10-02 10:55:03 +0200

    webshop

---
+++ First.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello, World!<h1>
+        <p>This is my first PHP file.</p>
+        <?php
+        for ($i=1; $i <=1111; $i++) {
+            print "Hello world<br>";
+        }
+        ?>
+</body>
+</html>
