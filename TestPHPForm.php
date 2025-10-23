f1a53ed1a5caa11fbabfd689f0a03000766e1a5a
mybaby <example.com>
2025-10-23 11:47:55 +0200

    1

--- TestPHPForm.php
+++ TestPHPForm.php
@@ -1,38 +1,38 @@
-<!DOCTYPE html>
+
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
-
 <body>
-    <?php
-    $carsAavailable = ["Volvo", "BMW", "Toyota", "Honda", "Mercedes", "Opel"];
 
-    if (isset($_GET["lname"], $_GET["cars"])) {
-        print("User " . $_GET["fname"] . " loves " . $_GET["cars"]);
+<?php
+$carsAvailable = ["Volvo", "Saab", "Mercedes", "Audi", "Dacia"];
+
+if (isset($_POST["fname"], $_POST["lname"], $_POST["cars"])) {
+    if (!isset($carsAvailable[$_POST["cars"]])) {
+        PRINT ("ha ha + youare a hacker ! Stop bothering me");
+        die();
     }
-    ?>
+    print("User " . $_POST["fname"] . " loves " . $_POST["cars"]);
+}
+?>
 
-    <form>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname"><br>
-        Pick your favorite car:
-        <select name="cars" id="cars">
-            <?php
-            foreach ($carsAavailable as $i => $car ) {
-            ?>
-                <option value="<?= $i ?>"> <?= $car ?> </option>
-            <?php
-            }
-            ?>
-        </select><br>
-        <input type="submit" value="Submit">
-    </form>
+<form method="POST">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname"><br>
+    Pick your favourite car:
+    <select name="cars" id="cars">
+        <?php
+        foreach ($carsAvailable as $i => $car) {
+        ?>
+            <option value="<?= $i ?>"><?= $car ?></option>
+        <?php
+        }
+        ?>
+    </select><br>
+    <input type="submit" value="Submit">
+</form>
+
 </body>
-
 </html>
